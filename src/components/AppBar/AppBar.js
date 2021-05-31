4ae74c73f0280679b65bef4866b419f3aa2ba2e4
Ridwan <myworld.shoyab@gmail.com>
2021-05-31 12:48:21 +0600

    appbar completed-2.0

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 import "./AppBar.css";
-import Nav1 from '../../img/nav-1.png';
-import Nav2 from '../../img/nav-2.png';
-import Nav3 from '../../img/nav-3.png';
-import Nav4 from '../../img/nav-4.png';
-import Nav5 from '../../img/nav-5.png';
+import Nav1 from "../../img/nav-1.png";
+import Nav2 from "../../img/nav-2.png";
+import Nav3 from "../../img/nav-3.png";
+import Nav4 from "../../img/nav-4.png";
+import Nav5 from "../../img/nav-5.png";
 
 const AppBar = () => {
-   return <div class="circular-menu active">
- <menu class="items-wrapper">
-    <img src={Nav1} alt="kmm" class="menu-item" />
-    <img src={Nav2} alt="kdj" class="menu-item" />
-    <img src={Nav3} alt="kjf" class="menu-item" />
-    <img src={Nav4} alt="kdf" class="menu-item" />
-    <img src={Nav5} alt="kdf" class="menu-item" />
-  </menu>
+  return (
+    <div className="circular-menu active">
+      <menu className="items-wrapper">
+        <img src={Nav1} alt="kmm" className="menu-item" />
+        <img src={Nav2} alt="kdj" className="menu-item" />
+        <img src={Nav3} alt="kjf" className="menu-item" />
+        <img src={Nav4} alt="kdf" className="menu-item" />
+        <img src={Nav5} alt="kdf" className="menu-item" />
+      </menu>
     </div>
-   
+  );
 };
 
 export default AppBar;
